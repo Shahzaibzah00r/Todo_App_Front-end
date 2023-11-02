34ca990395a89b23afdb0bfcf4a6dfe671031882
Shahzaibzah00r <shahzaibzahoor7@gmail.com>
2023-11-02 17:49:31 +0500

    pic add in header

--- src/My_Componets/Header.js
+++ src/My_Componets/Header.js
@@ -1,53 +1,4 @@
-// import Button from "react-bootstrap/Button";
-// import Container from "react-bootstrap/Container";
-// import Form from "react-bootstrap/Form";
-// import Nav from "react-bootstrap/Nav";
-// import Navbar from "react-bootstrap/Navbar";
-// import NavDropdown from "react-bootstrap/NavDropdown";
-
-// function Header() {
-//   return (
-//     <Navbar expand="lg" className="bg-body-tertiary">
-//       <Container fluid>
-//         <Navbar.Brand href="#">Navbar scroll</Navbar.Brand>
-//         <Navbar.Toggle aria-controls="navbarScroll" />
-//         <Navbar.Collapse id="navbarScroll">
-//           <Nav
-//             className="me-auto my-2 my-lg-0"
-//             style={{ maxHeight: "100px" }}
-//             navbarScroll
-//           >
-//             <Nav.Link href="#action1">Home</Nav.Link>
-//             <Nav.Link href="#action2">Link</Nav.Link>
-//             <NavDropdown title="Link" id="navbarScrollingDropdown">
-//               <NavDropdown.Item href="#action3">Action</NavDropdown.Item>
-//               <NavDropdown.Item href="#action4">
-//                 Another action
-//               </NavDropdown.Item>
-//               <NavDropdown.Divider />
-//               <NavDropdown.Item href="#action5">
-//                 Something else here
-//               </NavDropdown.Item>
-//             </NavDropdown>
-//             <Nav.Link href="#" disabled>
-//               Link
-//             </Nav.Link>
-//           </Nav>
-//           <Form className="d-flex">
-//             <Form.Control
-//               type="search"
-//               placeholder="Search"
-//               className="me-2"
-//               aria-label="Search"
-//             />
-//             <Button variant="outline-success">Search</Button>
-//           </Form>
-//         </Navbar.Collapse>
-//       </Container>
-//     </Navbar>
-//   );
-// }
-import { Container, Nav, Navbar, NavDropdown, Form } from "react-bootstrap";
+import { Container, Nav, Navbar, NavDropdown, NavItem } from "react-bootstrap";
 import { Link, useNavigate } from "react-router-dom";
 import "../My_Componets/links.css";
 import { useEffect, useState } from "react";
@@ -73,7 +24,7 @@ function Header() {
     }
   }, [btnLogout, setLcData, localData]);
   return (
-    <Navbar expand="lg" style={{ color: "black" }} variant="dark" bg="dark">
+    <Navbar expand="lg" variant="dark" bg="dark">
       <Container fluid>
         <Navbar.Brand href="/">
           <Link to="/" className="links">
@@ -96,21 +47,36 @@ function Header() {
             )}
           </Nav>
 
-          <Nav className=" me-2 " style={{ maxHeight: "100px" }} navbarScroll>
+          <Nav className=" me-5 " style={{ maxHeight: "100px" }} navbarScroll>
             {localData ? (
-              <NavDropdown
-                title={lcData}
-                id="navbarScrollingDropdown"
-                className=" me-5 "
-              >
-                <NavDropdown.Item href="profile">
-                  <Link to="/profile" className="links navItems">
-                    My Profile
-                  </Link>
-                </NavDropdown.Item>
-                <NavDropdown.Divider />
-                <NavDropdown.Item onClick={btnLogout}>Logout</NavDropdown.Item>
-              </NavDropdown>
+              <>
+                <NavItem className=" me-2 my-auto">
+                  <img
+                    src={localData.userData.profilePic}
+                    style={{
+                      height: "35px",
+                      width: "35px",
+                      cursor: "pointer",
+                      borderRadius: "100%",
+                    }}
+                  />
+                </NavItem>
+                <NavDropdown
+                  title={lcData}
+                  id="navbarScrollingDropdown"
+                  className=" me-5 "
+                >
+                  <NavDropdown.Item href="profile">
+                    <Link to="/profile" className="links navItems">
+                      My Profile
+                    </Link>
+                  </NavDropdown.Item>
+                  <NavDropdown.Divider />
+                  <NavDropdown.Item onClick={btnLogout}>
+                    Logout
+                  </NavDropdown.Item>
+                </NavDropdown>
+              </>
             ) : (
               <Nav>
                 <Nav.Link href="/login">
