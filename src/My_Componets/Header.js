a14d1d415f42251c265a608365cdf140ccc2a137
Shahzaibzah00r <shahzaibzahoor7@gmail.com>
2023-11-02 17:23:54 +0500

    dropDwon items fixed in mobile view

--- src/My_Componets/Header.js
+++ src/My_Componets/Header.js
@@ -1,3 +1,52 @@
+// import Button from "react-bootstrap/Button";
+// import Container from "react-bootstrap/Container";
+// import Form from "react-bootstrap/Form";
+// import Nav from "react-bootstrap/Nav";
+// import Navbar from "react-bootstrap/Navbar";
+// import NavDropdown from "react-bootstrap/NavDropdown";
+
+// function Header() {
+//   return (
+//     <Navbar expand="lg" className="bg-body-tertiary">
+//       <Container fluid>
+//         <Navbar.Brand href="#">Navbar scroll</Navbar.Brand>
+//         <Navbar.Toggle aria-controls="navbarScroll" />
+//         <Navbar.Collapse id="navbarScroll">
+//           <Nav
+//             className="me-auto my-2 my-lg-0"
+//             style={{ maxHeight: "100px" }}
+//             navbarScroll
+//           >
+//             <Nav.Link href="#action1">Home</Nav.Link>
+//             <Nav.Link href="#action2">Link</Nav.Link>
+//             <NavDropdown title="Link" id="navbarScrollingDropdown">
+//               <NavDropdown.Item href="#action3">Action</NavDropdown.Item>
+//               <NavDropdown.Item href="#action4">
+//                 Another action
+//               </NavDropdown.Item>
+//               <NavDropdown.Divider />
+//               <NavDropdown.Item href="#action5">
+//                 Something else here
+//               </NavDropdown.Item>
+//             </NavDropdown>
+//             <Nav.Link href="#" disabled>
+//               Link
+//             </Nav.Link>
+//           </Nav>
+//           <Form className="d-flex">
+//             <Form.Control
+//               type="search"
+//               placeholder="Search"
+//               className="me-2"
+//               aria-label="Search"
+//             />
+//             <Button variant="outline-success">Search</Button>
+//           </Form>
+//         </Navbar.Collapse>
+//       </Container>
+//     </Navbar>
+//   );
+// }
 import { Container, Nav, Navbar, NavDropdown, Form } from "react-bootstrap";
 import { Link, useNavigate } from "react-router-dom";
 import "../My_Componets/links.css";
@@ -18,32 +67,26 @@ function Header() {
   const localData = JSON.parse(localStorage.getItem("newData"));
   useEffect(() => {
     if (localData) {
-      setLcData(localData.userData.fName);
+      const firstLtr = localData.userData.fName.slice(0, 1).toUpperCase();
+      const fullName = firstLtr + localData.userData.fName.slice(1);
+      setLcData(fullName);
     }
   }, [btnLogout, setLcData, localData]);
   return (
     <Navbar expand="lg" style={{ color: "black" }} variant="dark" bg="dark">
-      <Container className="d-flex">
+      <Container fluid>
         <Navbar.Brand href="/">
           <Link to="/" className="links">
             To-Do-APP
           </Link>
         </Navbar.Brand>
         <Navbar.Toggle aria-controls="navbarScroll" />
-        <Navbar.Collapse
-          id="navbarScroll"
-          className=" d-flex justify-content-between"
-        >
-          <Nav>
+        <Navbar.Collapse id="navbarScroll">
+          <Nav className=" me-auto ms-4 my-lg-0">
             {localData ? (
               <>
-                {/* <Nav.Link href="/">
-                  <Link to="/" className="links navItems">
-                    Home
-                  </Link>
-                </Nav.Link> */}
                 <Nav.Link href="/mynotes">
-                  <Link to="/mynotes" className="links navItems">
+                  <Link to="/mynotes" className="links navItems ">
                     My-Notes
                   </Link>
                 </Nav.Link>
@@ -52,13 +95,14 @@ function Header() {
               ""
             )}
           </Nav>
-          <Nav
-            className=" my-2 my-lg-0 "
-            style={{ maxHeight: "100px" }}
-            navbarScroll
-          >
+
+          <Nav className=" me-2 " style={{ maxHeight: "100px" }} navbarScroll>
             {localData ? (
-              <NavDropdown title={lcData} id="navbarScrollingDropdown">
+              <NavDropdown
+                title={lcData}
+                id="navbarScrollingDropdown"
+                className=" me-5 "
+              >
                 <NavDropdown.Item href="profile">
                   <Link to="/profile" className="links navItems">
                     My Profile
