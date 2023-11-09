a59d0f4d81945100c21dceab5bfdf6fe5444c4e1
Shahzaibzah00r <shahzaibzahoor7@gmail.com>
2023-11-09 13:11:10 +0500

    add google btn

--- src/My_Componets/Users/Login.js
+++ src/My_Componets/Users/Login.js
@@ -6,6 +6,8 @@ import { Link, useNavigate } from "react-router-dom";
 import axios from "axios";
 import Loading from "../Loader/Loading";
 import Errors from "../Errors/Errors";
+import "./Login.css";
+import { useGoogleLogin } from "@react-oauth/google";
 
 const Login = () => {
   const navigate = useNavigate();
@@ -14,19 +16,43 @@ const Login = () => {
   const [isError, setIsError] = useState(null);
   const [loading, setLoading] = useState(null);
 
-  useEffect(() => {
-    const info = localStorage.getItem("newData");
-    if (info) {
-      navigate("/Mynotes");
-    }
-  }, []);
-
   const showAlert = (message, type) => {
     setIsError({ message: message, type: type });
     setTimeout(() => {
       setIsError(null);
     }, 1500);
   };
+
+  const login = useGoogleLogin({
+    onSuccess: async (tokenResponse) => {
+      try {
+        setLoading(true);
+        const loginGoogle = await axios.post(
+          "http://localhost:5000/loginGoogle",
+          // "https://todoapplication.up.railway.app/updateProfle",
+          "https://todoapplication.up.railway.app/loginGoogle",
+          tokenResponse,
+          { headers: { Authorization: tokenResponse.access_token } }
+        );
+        setLoading(false);
+        if (loginGoogle) {
+          showAlert("User logged in", "success");
+          navigate("/Mynotes");
+          localStorage.setItem("newData", JSON.stringify(loginGoogle.data));
+        }
+      } catch (error) {
+        showAlert("Invalid credentials", "danger");
+      }
+    },
+    onError: (errorResponse) => showAlert("Invalid credentials", "danger"),
+  });
+
+  useEffect(() => {
+    const info = localStorage.getItem("newData");
+    if (info) {
+      navigate("/Mynotes");
+    }
+  }, []);
 
   const handleSubmit = async (e) => {
     e.preventDefault();
@@ -62,55 +88,91 @@ const Login = () => {
     <MainContent
       title="LOGIN"
       child={
-        <div className="m-auto col-sm-12 col-md-10 col-lg-6 p-3 d-flex justify-content-center  border bg-light rounded">
-          <Form
-            onSubmit={handleSubmit}
-            method="POST"
-            className="loginBg w-100 mt-5 "
-            // className="loginBg mt-5  "
-            // size="sm"
-          >
-            {isError && (
-              <Errors child={isError.message} variant={isError.type} />
-            )}
-            <Form.Group className="mb-3 " controlId="formBasicEmail" size="sm">
-              <Form.Label>Email address</Form.Label>
-              <Form.Control
-                required
-                onChange={(e) => setEmail(e.target.value)}
-                type="email"
-                value={email}
-                placeholder="Enter email"
-                className="w-40"
-                style={{ backgroundColor: "#f7efef" }}
-              />
-            </Form.Group>
-            <Form.Group className="mb-3" controlId="formBasicPassword">
-              <Form.Label>Password</Form.Label>
-              <Form.Control
-                required
-                onChange={(e) => setPassword(e.target.value)}
-                value={password}
-                type="password"
-                placeholder="Password"
-                style={{ backgroundColor: "#f7efef" }}
-              />
-            </Form.Group>
-            <div className=" d-flex align-items-center ">
-              <Button
-                variant="primary"
-                type="submit"
-                className="mb-3 me-3 d-flex "
-              >
-                Submit
-                {loading && <Loading size="sm" />}
-              </Button>
+        <section className="vh-80">
+          <div className="container-fluid h-custom">
+            <div className="row d-flex justify-content-center align-items-center h-100">
+              <div className="col-md-9 col-lg-6 col-xl-5">
+                <img
+                  src="https://mdbcdn.b-cdn.net/img/Photos/new-templates/bootstrap-login-form/draw2.webp"
+                  className="img-fluid"
+                  alt="Sample image"
+                />
+              </div>
+              <div className="col-md-8 col-lg-6 col-xl-4 offset-xl-1 py-2">
+                <div className="socialLink">
+                  <a
+                    type="button"
+                    onClick={() => login()}
+                    className="btn btn-block   d-flex my-3 align-items-center justify-content-center  "
+                    style={{ backgroundColor: "#dd4b39", color: "white" }}
+                  >
+                    <i className="fa fa-google mx-2 "></i> Sign in with Google
+                  </a>
+                </div>
+                <div>
+                  <a
+                    className="btn btn-block   d-flex my-3 align-items-center justify-content-center "
+                    style={{ backgroundColor: "#464646", color: "white" }}
+                    // type="submit"
+                  >
+                    <i className="fa fa-github mx-2"></i>Sign in with GitHub
+                  </a>
+                </div>
+                <div className="divider d-flex align-items-center my-4">
+                  <p className="text-center fw-bold mx-3 mb-0 text-muted">OR</p>
+                </div>
+
+                <Form onSubmit={handleSubmit} method="POST">
+                  {isError && (
+                    <Errors child={isError.message} variant={isError.type} />
+                  )}
+
+                  <Form.Group
+                    className="mb-4 "
+                    controlId="formBasicEmail"
+                    size="sm"
+                  >
+                    <Form.Label>Email address</Form.Label>
+                    <Form.Control
+                      required
+                      onChange={(e) => setEmail(e.target.value)}
+                      type="email"
+                      className="mb-3"
+                      value={email}
+                      placeholder="Enter email"
+                      style={{ backgroundColor: "#f7efef" }}
+                    />
+                  </Form.Group>
+                  <Form.Group className="mb-3" controlId="formBasicPassword">
+                    <Form.Label>Password</Form.Label>
+                    <Form.Control
+                      required
+                      onChange={(e) => setPassword(e.target.value)}
+                      value={password}
+                      type="password"
+                      placeholder="Password"
+                      style={{ backgroundColor: "#f7efef" }}
+                    />
+                  </Form.Group>
+
+                  <div className="d-flex  ">
+                    <Button
+                      variant="primary"
+                      type="submit"
+                      className="mb-3 me-3 d-flex"
+                    >
+                      Login
+                      {loading && <Loading size="sm" />}
+                    </Button>
+                  </div>
+                  <div>
+                    No account? <Link to="/register">Create one!</Link>
+                  </div>
+                </Form>
+              </div>
             </div>
-            <div>
-              No account? <Link to="/register">Create one!</Link>
-            </div>
-          </Form>
-        </div>
+          </div>
+        </section>
       }
     />
   );
--- src/index.js
+++ src/index.js
@@ -4,12 +4,18 @@ import App from "./App";
 import "../node_modules/bootstrap/dist/js/bootstrap";
 import "../node_modules/bootstrap/dist/css/bootstrap.min.css";
 import { BrowserRouter } from "react-router-dom";
+import { GoogleOAuthProvider } from "@react-oauth/google";
+// import GitHubLogin from "react-github-login";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-  <React.StrictMode>
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
-  </React.StrictMode>
+  <GoogleOAuthProvider clientId="703684881995-4e6k4frkpts7cie8csh3mau17e2v1no9.apps.googleusercontent.com">
+    {/* <GitHubLogin clientId> */}
+    <React.StrictMode>
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+    </React.StrictMode>
+    {/* </GitHubLogin> */}
+  </GoogleOAuthProvider>
 );
